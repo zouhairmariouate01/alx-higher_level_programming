343876c666e108b9da5cebf2847df8ce33d2fd54
zouhairmariouate01 <zouhairmarioute@gmail.com>
2024-01-02 15:26:27 -0800

    py1

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for char in range(97, 123):
-    print(chr(char), end='')
+#!/usr/bin/python3
+# 2-print_alphabet.py
+
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# 4-print_hexa.py
+
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
