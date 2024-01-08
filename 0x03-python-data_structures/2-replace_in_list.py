f136e7d878803d5a2dcda15d8701dcb74ef16299
zouhairmariouate01 <zouhairmarioute@gmail.com>
2024-01-08 00:57:40 -0800

    py3

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 2-replace_in_list.py
+
+
+def replace_in_list(my_list, idx, element):
+    """Substitute an element in a list at a particular position."""
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
