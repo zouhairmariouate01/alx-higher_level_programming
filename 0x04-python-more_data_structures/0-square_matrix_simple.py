45489ee348a0a34f9409d5b5093c7ee0b19b59ab
zouhairmariouate01 <zouhairmarioute@gmail.com>
2024-01-10 12:18:57 -0800

    py4

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    # Iterate through each row of the input matrix
-    for i in range(len(matrix)):
-        # Apply the square operation to each value in the current row
-        matrix[i] = [value ** 2 for value in matrix[i]]
 
-    return( matrix)
+def square_matrix_simple(matrix=[]):
+    result_matrix = [[0] * len(matrix[0]) for _ in range(len(matrix))]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0]))
+        result_matrix[i][j] = matrix[i][j] ** 2
+
+        return (result_matrix)
