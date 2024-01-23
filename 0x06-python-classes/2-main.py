606b27ed175cd3caae6be41ca417d23df0133269
zouhairmariouate01 <zouhairmarioute@gmail.com>
2024-01-23 12:58:08 -0800

    tsk

--- 0x06-python-classes/2-main.py
+++ 0x06-python-classes/2-main.py
@@ -1,34 +1,23 @@
 #!/usr/bin/python3
-Square = __import__('2-square').Square
+# 0-square.py by Ehoneah Obed
+"""A module that defines a square """
 
-my_square_1 = Square(3)
-print(type(my_square_1))
-print(my_square_1.__dict__)
 
-my_square_2 = Square()
-print(type(my_square_2))
-print(my_square_2.__dict__)
+class Square:
+    """A class that represents a square"""
 
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
 
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
 
-try:
-    my_square_3 = Square("3")
-    print(type(my_square_3))
-    print(my_square_3.__dict__)
-except Exception as e:
-    print(e)
-
-try:
-    my_square_4 = Square(-89)
-    print(type(my_square_4))
-    print(my_square_4.__dict__)
-except Exception as e:
-    print(e)
+        self.__size = size
