5db8b06461ebf54f294429f77fdc94c4fb672525
zouhairmariouate01 <zouhairmarioute@gmail.com>
2024-03-12 16:34:14 -0700

    task

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- Insert values
--- Execute: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+-- Inserts a new row into the table first_table in my MySQL server.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
